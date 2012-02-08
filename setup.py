39e883d5cb7ce0dec646ae9a537731aeedc9490f
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-02-08 20:57:38 +0200

    file deletend and dependencies updated

--- setup.py
+++ setup.py
@@ -1,46 +1,45 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = read('collective', 'pfg', 'showrequest', 'version.txt')[:-1]
 
 long_description = (
     open("README.txt").read() + "\n" +
     open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
     open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
     open(os.path.join("docs", "CREDITS.txt")).read()
-    )
+)
 
-setup(name='collective.pfg.showrequest',
-      version=version,
-      description="Show previous form input values in viewlet for PloneFormGen.",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+
+setup(
+    name='collective.pfg.showrequest',
+    version='0.3',
+    description="Show previous form input values in viewlet for PloneFormGen.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Taito Horiuchi',
-      author_email='taito.horiuchi@gmail.com',
-      url='http://pypi.python.org/pypi/collective.pfg.showrequest',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.pfg'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.PloneFormGen',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@gmail.com',
+    url='https://github.com/taito/collective.pfg.showrequest',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.pfg'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.PloneFormGen',
+        'setuptools',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
